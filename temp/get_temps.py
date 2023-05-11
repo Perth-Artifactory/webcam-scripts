8a397c955b1c95fe0124c243b55946c5a3e029fd
Fletcher <fletcher.boyd@artifactory.org.au>
2023-05-11 13:34:59 +0800

    Get temps from home assistant

---
+++ temp/get_temps.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+# Get temp data from Home Assistant
+# Fletcher Boyd (fletcher.boyd@artifactory.org.au)
+
+import requests
+from pprint import pprint
+
+token = "Bearer TOKEN"
+
+sensors = {"name": "entity_id",
+           "second_name": "entity_id"}
+
+
+url = "http://home.assistant:8123/api/states/"
+headers = {
+    "Authorization": token,
+    "content-type": "application/json",
+}
+
+data = {}
+for sensor in sensors:
+    r = requests.get(url+sensors[sensor], headers=headers)
+    t = r.json()["state"]
+    data[sensor] = round(float(t),1)
+
+pprint(data)
