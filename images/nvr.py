97093f52694efc401b54d47b4765a2c79a53ca7f
Fletcher <me@fboyd.me>
2025-04-13 13:52:41 +0800

    Fix logging message, gracefully handle None snapshots

--- images/nvr.py
+++ images/nvr.py
@@ -1,31 +1,33 @@
-import asyncio
 import json
+import asyncio
 import logging
+import time
+from uiprotect import ProtectApiClient
 import os
 import shutil
-import time
-
-from uiprotect import ProtectApiClient
 
 
 class EpochTimeFormatter(logging.Formatter):
     def formatTime(self, record, datefmt=None):
         return str(int(time.time()))
 
-logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] %(message)s')
+
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s [%(levelname)s] %(message)s"
+)
 for handler in logging.getLogger().handlers:
-    handler.setFormatter(EpochTimeFormatter('%(asctime)s [%(levelname)s] %(message)s'))
+    handler.setFormatter(EpochTimeFormatter("%(asctime)s [%(levelname)s] %(message)s"))
 
 # Load config from file
 with open("config.json", "r") as f:
     config: dict = json.load(f)
 if not config["path"].endswith("/"):
     config["path"] += "/"
-    
+
 # Check for --all
 if "--all" in os.sys.argv:
     config["cameras"]["ignored"] = []
-    logging.info("Forcing all ignored cameras to be included")
+    logging.info("Ignoring 'ignored' config")
 
 protect = ProtectApiClient(
     host=config["nvr"]["host"],
@@ -37,14 +39,16 @@ protect = ProtectApiClient(
 
 disabled = []
 
+
 async def list_cameras():
     """List all cameras and update snapshots"""
-    
+
     await protect.update()
 
     global disabled
     disabled = []
     for camera in protect.bootstrap.cameras.values():
+        print(camera.name)
         if camera.name in config["cameras"]["ignored"]:
             logging.info(f"IGNORE {camera.name}")
             continue
@@ -54,14 +58,22 @@ async def list_cameras():
             disabled.append(camera.name)
             continue
         snapshot = await protect.get_camera_snapshot(camera.id)
-        with open(f"{config['path']}{camera.name}.jpg", "wb") as f:
-            logging.info(f"WRITING {camera.name}")
-            f.write(snapshot)
-            
+
+        if snapshot:
+            with open(f"{config['path']}{camera.name}.jpg", "wb") as f:
+                logging.info(f"WRITING {camera.name}")
+                f.write(snapshot)
+        else:
+            logging.info(f"FAILED {camera.name}")
+            shutil.copy("./images/offline.jpg", f"{config['path']}{camera.name}.jpg")
+            disabled.append(camera.name)
+
+
 async def close():
     await protect.async_disconnect_ws()
     await protect._session.close()
-    
+
+
 asyncio.run(list_cameras())
 
 for camera in config["cameras"]["disabled"]:
